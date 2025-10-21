d795df7c224aa5c951a7b33fe4224c79ef961697
medlaq777 <m.laqsoumi@gmail.com>
2025-10-21 14:08:02 +0100

    complet structer setup

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import 'dotenv';
+import express from 'express';
+import cors from "cors"
+import Config from "./config/config.ts"
+import DB from './config/db.ts';
+
+const app = express();
+const port = Config.port;
+
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://localhost:3000'],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowHeader: ["Content-Type", "Authorization"],
+  credentials: true
+}
+app.use(cors(corsOptions));
+app.options('/api', cors(corsOptions));
+app.disable('x-powred-by');
+app.use(express.json());
+DB.getInstance();
+
+app.use((err: any, res: express.Response) => {
+  console.error(err);
+  const status: number = err.status || 500;
+  res.status(status).json({
+    message: err.message || 'Internal Server Error',
+    status: status
+  });
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+})
