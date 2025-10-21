612fa88b9b611b5367882a32ce5ff2de13154985
medlaq777 <m.laqsoumi@gmail.com>
2025-10-21 16:15:34 +0100

    DAR-42: update the hash utils

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,10 +1,14 @@
 import bcrypt from "bcryptjs";
 
-export const hash = async (plain: string) => {
-  const salt = await bcrypt.genSalt(12);
-  return bcrypt.hash(plain, salt);
-};
+class HashProcess {
+  async hash(plain: string) {
+    const salt = await bcrypt.genSalt(12);
+    return bcrypt.hash(plain, salt)
+  }
 
-export const compare = async (plain: string, hashed: string) => {
-  return bcrypt.compare(plain, hashed);
-};
+  async compare(plain: string, hashed: string) {
+    return bcrypt.compare(plain, hashed)
+  }
+}
+
+export default new HashProcess();
