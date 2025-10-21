b395c7a23a210d08049aeb5efc300823658afd1a
medlaq777 <m.laqsoumi@gmail.com>
2025-10-21 16:11:54 +0100

    DAR-39: Update the prisma singleton db connection

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,11 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var __prisma: PrismaClient | undefined;
+class PrismaSingleton {
+  private static instance: PrismaClient;
+
+  private constructor() { }
+
+  public static getInstance(): PrismaClient {
+    if (!PrismaSingleton.instance) {
+      PrismaSingleton.instance = new PrismaClient();
+      if (process.env.NODE_ENV !== "production") {
+        (global as any).__prisma = PrismaSingleton.instance;
+      }
+    }
+
+    return PrismaSingleton.instance;
+  }
 }
 
-const prisma = global.__prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") global.__prisma = prisma;
-
+const prisma = PrismaSingleton.getInstance();
 export default prisma;
