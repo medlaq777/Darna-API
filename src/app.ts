73f0aa3930354715ef6f281e76383d950fbf0a1f
Taha Mlaiki <mlaikitaha29@gmail.com>
2025-10-21 10:02:22 +0100

    DAR-40 <message>

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,34 @@
-import 'dotenv';
-import express from 'express';
-import cors from "cors"
-import Config from "./config/config.ts"
-import DB from './config/db.ts';
+import "dotenv";
+import express from "express";
+import cors from "cors";
+import Config from "./config/config.ts";
+import DB from "./config/db.ts";
 
 const app = express();
 const port = Config.port;
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:3000'],
+  origin: ["http://localhost:3000", "http://localhost:3000"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowHeader: ["Content-Type", "Authorization"],
-  credentials: true
-}
+  credentials: true,
+};
 app.use(cors(corsOptions));
-app.options('/api', cors(corsOptions));
-app.disable('x-powred-by');
+app.options("/api", cors(corsOptions));
+app.disable("x-powred-by");
 app.use(express.json());
 DB.getInstance();
+
+console.log("taha mlaiki");
 
 app.use((err: any, res: express.Response) => {
   console.error(err);
   const status: number = err.status || 500;
   res.status(status).json({
-    message: err.message || 'Internal Server Error',
-    status: status
+    message: err.message || "Internal Server Error",
+    status: status,
   });
 });
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
-})
+});
