934d386d87dc836fac38eb16a4d6e5f313091b5f
medlaq777 <m.laqsoumi@gmail.com>
2025-10-22 14:11:21 +0100

    DAR-38 update the src database schema

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,22 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
-class PrismaSingleton {
+class Prisma {
   private static instance: PrismaClient;
 
   private constructor() { }
 
   public static getInstance(): PrismaClient {
-    if (!PrismaSingleton.instance) {
-      PrismaSingleton.instance = new PrismaClient();
+    if (!Prisma.instance) {
+      Prisma.instance = new PrismaClient();
       if (process.env.NODE_ENV !== "production") {
-        (global as any).__prisma = PrismaSingleton.instance;
+        (global as any).__prisma = Prisma.instance;
       }
     }
 
-    return PrismaSingleton.instance;
+    return Prisma.instance;
   }
 }
 
-const prisma = PrismaSingleton.getInstance();
+const prisma = Prisma.getInstance();
 
 export default prisma;
