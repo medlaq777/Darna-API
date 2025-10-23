92d8a50f5012e4a071b0b7abf11c44db0ce4aae8
medlaq777 <m.laqsoumi@gmail.com>
2025-10-23 16:29:43 +0100

    DAR-45: Add GOOGLE api For Google provider and read it from.env in the config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,7 @@ export const config = {
   JWTEXPIRESIN: process.env.JWT_EXPIRES_IN,
   EMAIL_USER: process.env.MAIL_USER,
   EMAIL_PASS: process.env.MAIL_PASS,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GOOGLE_CLIENT_CALLBACK_URL: process.env.GOOGLE_CLIENT_CALLBACK_URL,
 };
